d9bc96bfc00d5075b928be81956717224392f703
Abdul Hossain <abdulhossain925@gmail.com>
2025-09-02 01:38:29 -0400

    first commit

---
+++ src/config/footer.js
@@ -0,0 +1,38 @@
+export const footerSocials = [
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/company/deepline-systems/?viewAsMember=true',
+    icon: "mdi:linkedin",
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/deeplineai',
+    icon: "mdi:twitter",
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/DeepLine-Systems',
+    icon: "mdi:github",
+  },
+  {
+    name: 'Discord',
+    url: 'https://discord.gg/AX9YF3Xa',
+    icon: "ic:baseline-discord",
+  },
+]
+
+export const footerLists = [
+	{
+		title: 'Deepline',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `DeepLine`,
+	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
+	name: `Deepline`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
